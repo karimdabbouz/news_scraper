2fbc6e67a3c5c05e1e6eb0934e94559afed305f4
karimdabbouz <hey@karim.ooo>
2024-10-30 18:29:35 +0100

    first commit

---
+++ news_scraper/__init__.py
@@ -0,0 +1,6 @@
+# from .knowledge_graph import KnowledgeGraph
+# from .topic_modeling import Bertopic, SklearnClustering
+# from .loading_cleaning import ArticleLoader
+# from .embeddings import TransformerEmbeddings
+
+# __all__ = ['KnowledgeGraph', 'Bertopic', 'SklearnClustering', 'ArticleLoader', 'TransformerEmbeddings']
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='news_scraper',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author='Karim Dabbouz',
+    author_email='hey@karim.ooo',
+    description='A Python with utils to scrape articles from any news outlet.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/karimdabbouz/news_scraper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
